0317b98152e4f5705ac5f413588364c593ccc5af
ramaafriandi25 <ramaafriandi25@gmail.com>
2024-12-16 21:04:09 +0700

    Update and rename main.c to projrct akhir pemlan kelompok 5.c

---
+++ projrct akhir pemlan kelompok 5.c
@@ -0,0 +1,17 @@
+//penulisan mantra ajaib bahasa c
+
+//struct untuk data barang
+
+//data barang
+
+//void untuk menampilkan data barang
+
+// void untuk melakukan bubble sort
+
+//void untuk selection sort
+
+//void untuk insertions sort
+
+// syntax untuk melakukan jump search
+
+// syntax utama
