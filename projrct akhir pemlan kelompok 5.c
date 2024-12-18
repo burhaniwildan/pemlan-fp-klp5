815d009087c7a2d89a6b7b5e5a1690afbdf65f00
Caezarlov nugraha <85265087+CATSSRIN@users.noreply.github.com>
2024-12-18 19:53:36 +0700

    Update projrct akhir pemlan kelompok 5.c

--- projrct akhir pemlan kelompok 5.c
+++ projrct akhir pemlan kelompok 5.c
@@ -1,3 +1,40 @@
+#include <conio.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define max_lines 100
+
+typedef struct
+{
+    char id[15];
+    char nama[50];
+    char kategori[30];
+    int stok;
+} Barang;
+
+void printDataBarang(Barang brg[], int n) {
+    printf("+-------------------+-----------------------------------+-----------------------+--------+\n");
+    printf("| ID                | NAMA BARANG                       |KATEGORI               |STOK    |\n");
+    printf("+-------------------+-----------------------------------+-----------------------+--------+\n");
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("| %-17s | %-33s | %-21s | %-6d |\n", brg[i].id, brg[i].nama, brg[i].kategori, brg[i].stok);
+    }
+    printf("+-------------------+-----------------------------------+-----------------------+--------+\n");
+    printf("\n");
+}
+
+
+
+
+
+
+
+
+
+
 //penulisan mantra ajaib bahasa c
 
 //struct untuk data barang
